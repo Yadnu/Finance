6901f60a8b31443fbaccca5e48cda60db9e1ab3c
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-09-24 11:16:14 +0530

    Backend

--- finos/app/api/[[...route]]/route.ts
+++ finos/app/api/[[...route]]/route.ts
@@ -4,9 +4,13 @@ import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { error } from 'console'
+import accounts from './accounts'
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
+
+const routes = app.route("/accounts", accounts);
+
 
 app.get('/hello', (c) => {
   return c.json({
@@ -30,6 +34,8 @@ app.get('/hello', (c) => {
             
         }
         )
-    })
-export const GET = handle(app)
-export const POST = handle(app)
+    });
+export const GET = handle(app);
+export const POST = handle(app);
+
+export type AppType = typeof routes;
